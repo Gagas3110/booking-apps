d401c53540100c03997dfe443668cda58c313df2
gagasw <gagas3110@gmail.com>
2024-05-07 09:29:55 +0700

    adding new screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:booking_apps/screen/home_screen.dart';
+import 'package:booking_apps/presentation/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
