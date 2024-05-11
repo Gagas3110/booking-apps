52b83b23bd1bcd93dcbcb1e31e5381ed41b712d4
Gagas Wijaksana <gagas3110@gmail.com>
2024-05-11 19:41:40 +0700

    feat : auth (done integrate with bloc, need adjustment but..)

--- lib/src/presentation/screen/splash_screen.dart
+++ lib/src/presentation/screen/splash_screen.dart
@@ -3,6 +3,7 @@ import 'package:booking_auth/application/login/auth_bloc.dart';
 import 'package:booking_auth/injection.dart';
 import 'package:booking_home/booking_home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
--- packages/booking_auth/lib/presentation/auth/screen/login_screen.dart
+++ packages/booking_auth/lib/presentation/auth/screen/login_screen.dart
@@ -1,73 +1,23 @@
-import 'package:booking_auth/presentation/auth/components/booking_button.dart';
-import 'package:booking_auth/presentation/auth/components/booking_button_style.dart';
-import 'package:booking_auth/presentation/auth/components/booking_textfield.dart';
+// // ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 import 'package:booking_home/booking_home.dart';
 
-class LoginScreen extends StatefulWidget {
+import '../../../application/login/login_bloc.dart';
+import '../../../injection.dart';
+import '../components/login_body.dart';
+
+
+
+class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey.shade100,
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(top: 100),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    const Text(
-                      'Welcome\nto BookingApps',
-                      style: TextStyle(fontSize: 30),
-                    ),
-                    Image.asset('assets/image/booking_logo.png',
-                        width: 40, height: 40),
-                  ],
-                ),
-              ),
-              const SizedBox(height: 10),
-              const Text(
-                'Easy Books for all people',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 100),
-              const BookingFormTextField(
-                hint: 'Username',
-              ),
-              const SizedBox(height: 20),
-              const BookingFormTextField(
-                hint: 'Password',
-                suffixIcon: Icon(
-                  Icons.visibility_off,
-                  color: Colors.grey,
-                ),
-              ),
-              const SizedBox(height: 40),
-              BookingButton(
-                onPressed: () {
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (context) => const HomePage()));
-                },
-                title: 'Login',
-                style: PrimaryBookingButtonStyle.style,
-              ),
-            ],
-          ),
-        ),
-      ),
+    return BlocProvider(
+      create: (_) => getIt<LoginBloc>(),
+      child: const LoginBody(),
     );
   }
 }
+
