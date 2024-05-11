4e104eee7f73b51000b491c35c8a32381ac0ee3a
Gagas Wijaksana <gagas3110@gmail.com>
2024-05-11 19:41:40 +0700

    feat : auth (done integrate with bloc, need adjustment but..)

--- packages/booking_auth/lib/application/login/login_event.dart
+++ packages/booking_auth/lib/application/login/login_event.dart
@@ -1,7 +1,8 @@
-// import 'package:freezed_annotation/freezed_annotation.dart';
+part of 'login_bloc.dart';
 
-// @freezed
-// abstract class LoginEvent with _$LoginEvent {
-//   const factory LoginEvent.loginSubmitted(LoginReq model) = LoginSubmiited;
-//   const factory LogintEvent.checkLogin() = ChackLogin;
-// }
+@freezed
+abstract class LoginEvent with _$LoginEvent {
+  const factory LoginEvent.loginSubmitted(LoginRequest model) = LoginSubmiited;
+  const factory LoginEvent.checkLogin() = ChackLogin;
+  const factory LoginEvent.checkObsecure(bool obs) = CheckObs;
+}
