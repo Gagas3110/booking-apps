8f69adf319e3fd75202777ad7937f3fa81d7eb73
Gagas Wijaksana <gagas3110@gmail.com>
2024-05-11 10:37:36 +0700

    feat : booking auth ( without integration with bloc), soon as possible to integrate

---
+++ packages/booking_auth/lib/domain/core/utils/constant.dart
@@ -0,0 +1,11 @@
+const loginBaseUrlAuth = "https://dummyjson.com/auth/";
+
+// keys for sharedPref
+const cachedLogin = 'CACHED_LOGIN';
+
+// Error Messages for this packages
+
+const String serverFailureMessages = 'Server Failure, Please Try Again';
+const String cacheFailureMessages = 'Cache Failure, Please Try Again';
+const String defauktErrorMessages =
+    'Some Error happen on this apps, Please Try Again';
