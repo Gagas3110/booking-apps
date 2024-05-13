ac44bb5ecd14a8bf72b3769d3ce784482d7e00a2
Nafla Diva <nafladiva@gmail.com>
2024-05-13 17:34:52 +0700

    fix: dio injection

--- packages/booking_home/lib/src/injection.dart
+++ packages/booking_home/lib/src/injection.dart
@@ -1,5 +1,6 @@
 import 'package:booking_home/src/domain/i_home_repository.dart';
 import 'package:booking_home/src/infrastructure/data_sources/home_api.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
 import 'application/banner/banner_bloc.dart';
@@ -14,9 +15,9 @@ void configureBookingHomeDepedencies() {
   di.registerFactory(() => CategoryBloc(repository: di()));
   di.registerFactory(() => FlashSaleBloc(repository: di()));
 
-  // di.registerLazySingleton<Dio>(
-  //   () => Dio(),
-  // );
+  di.registerLazySingleton<Dio>(
+    () => Dio(),
+  );
   di.registerLazySingleton<HomeApi>(
     () => HomeApi(di()),
   );
